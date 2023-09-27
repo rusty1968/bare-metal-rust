6d07f10a622d1b646e92fcf05060dc45743b0fb2
Anthony Rocha <anthony.rocha@amd.com>
2023-09-27 00:21:48 -0700

    Bare Metal Rust project devcontainer.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-pub fn main() {
-    println!("Welcome, new Rustacean!");
+#![no_std]
+#![no_main]
+
+#[cfg(feature = "std")]
+pub fn main() {}
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
